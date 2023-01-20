3a225949dd8adf49b0f3fbd6a162a97a61fefe04
Your name <Your email>
2023-01-20 16:14:03 +0100

    test

--- routes/year.js
+++ routes/year.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-  const currentYear = new Date().getFullYear();
+  const currentYear = new Date().getFullYear().toString();
   res.json({ result:true, year:currentYear});
 });
 
